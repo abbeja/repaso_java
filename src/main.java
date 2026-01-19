3860edbd4e448327a444a4628d160b8d1442b65e
abbeja <henzogonzalez1@gmail.com>
2026-01-19 19:07:07 -0300

    repaso if

--- src/main.java
+++ src/main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class main {
+    
     public static void main(String[] args) {
-        System.out.println("hello world");
+    //    System.out.println("hello world");
+    String lluvia;
+    Scanner sc=new Scanner(System.in);
+    System.out.print("ta lloviendo: ");
+    lluvia=sc.nextLine();
+    if(lluvia.equals("si")){
+        System.out.println("si llueve usa paraguas");
+    }else{
+        System.out.println("si no sali asi no mas");
     }
+
+}
 }
